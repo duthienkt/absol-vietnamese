e6150286be854d3cd7c76b53c7b01b3934c78ef3
duthienkt <blueskythien2010@live.com>
2020-05-06 21:06:48 +0700

    - inport

--- dev.js
+++ dev.js
@@ -1,4 +1,8 @@
-var allWordDict = CommonWords.concat(UncommonWords).concat(RareWords).reduce(function (ac, cr) {
+import CommonWords from "./CommonWords";
+import UncommonWords from "./UncommonWords";
+import RareWords from "./RareWords";
+
+var allWordDict = CommonWords.concat(UncommonWords).concat(RareWordsWords).reduce(function (ac, cr) {
     ac[cr] = true;
     return ac;
 }, {})
