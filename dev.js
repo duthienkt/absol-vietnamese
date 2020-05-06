f5bfe762842d577092f1c38cdc3a1913fca40ed7
duthienkt <blueskythien2010@live.com>
2020-05-06 21:03:54 +0700

    - dev

---
+++ dev.js
@@ -0,0 +1,17 @@
+var allWordDict = CommonWords.concat(UncommonWords).concat(RareWords).reduce(function (ac, cr) {
+    ac[cr] = true;
+    return ac;
+}, {})
+
+var vietnamese = {
+    RareWords: RareWords,
+    CommonWords: CommonWords,
+    UncommonWords: UncommonWords,
+    allWordDict: allWordDict,
+    tokenizeByWord: function (text) {
+        return tokenizeByWord(text, allWordDict)
+    }
+}
+
+window.absol = window.absol ||{};
+window.absol.vietnamese = vietnamese;
